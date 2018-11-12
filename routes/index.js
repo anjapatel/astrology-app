25cf678928d2a6f5da09fb734d0cf98b2f8c5bb4
anjali.patel <anjali.patel@buzzfeed.com>
2018-11-12 15:57:22 +0100

    added user profile

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
@@ -15,11 +16,41 @@ router.get("/", ensureAuthenticated, (req, res) => {
 });
 
 router.get("/profile", ensureAuthenticated, (req, res) => {
-  res.render("user-profile", { user: req.user });
+  User.find().then(user => {
+    res.render("user-profile", { user: req.user });
+  });
 });
 
 router.get("/add-friend", ensureAuthenticated, (req, res) => {
   res.render("add-friend", { user: req.user });
+});
+
+router.get("/edit-profile", ensureAuthenticated, (req, res) => {
+  //const username = req.body.username;
+
+  User.findOne({ _id: req.query.user_id }).then(user => {
+    res.render("user-create-chart", { user });
+  });
+});
+
+router.post("/edit-profile", (req, res, next) => {
+  // const location = req.body.location;
+  // const birthday = req.body.birthday;
+  // const birthtime = req.body.birthtime;
+  const { location, birthday, birthtime } = req.body;
+  console.log(req.body);
+  User.updateOne(
+    { _id: req.user._id },
+    { $set: { location, birthday, birthtime } },
+    { new: true }
+  )
+    .then(user => {
+      res.redirect("/profile");
+      console.log("profile updated");
+    })
+    .catch(err => {
+      console.log("profile creation error");
+    });
 });
 
 /* Add Friend ========================================================== */
@@ -41,11 +72,9 @@ router.get("/add-friend", ensureAuthenticated, (req, res) => {
 // });
 
 // // uploadCloud.single('photo') is a middleware
-// // the parameter is 'photo' because we have 
+// // the parameter is 'photo' because we have
 // // --->  <input type="file" name="photo">
 // router.post('/movie/add', uploadCloud.single('photo'), (req, res, next) => {
-
-
 
 //   const newMovie = new Movie({
 //     title: req.body.title,
@@ -61,6 +90,5 @@ router.get("/add-friend", ensureAuthenticated, (req, res) => {
 //       console.log(error)
 //     })
 // });
-
 
 module.exports = router;
