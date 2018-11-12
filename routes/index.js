dfb42ebcd4d2aa4334800220058100649c78edf8
anjali.patel <anjali.patel@buzzfeed.com>
2018-11-12 11:26:23 +0100

    added routes and privacy pages to index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect("/auth/login");
+  }
+}
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", ensureAuthenticated, (req, res) => {
+  res.render("index", { user: req.user });
+});
+
+router.get("/profile", ensureAuthenticated, (req, res) => {
+  res.render("user-profile", { user: req.user });
+});
+
+router.get("/add-friend", ensureAuthenticated, (req, res) => {
+  res.render("add-friend", { user: req.user });
 });
 
 module.exports = router;
