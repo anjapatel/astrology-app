7c738fd052bc5967bbfed11b3dc99516796a8089
anjali.patel <anjali.patel@buzzfeed.com>
2018-11-12 12:10:49 +0100

    hi

--- routes/auth.js
+++ routes/auth.js
@@ -58,14 +58,6 @@ router.post("/signup", (req, res, next) => {
   });
 });
 
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  } else {
-    res.redirect("/auth/login");
-  }
-}
-
 router.get("/logout", (req, res) => {
   req.logout();
   res.redirect("/");
