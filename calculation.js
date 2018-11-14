3e51f5efc2b821e839cf633f973b20e7ec63e04c
anjali.patel <anjali.patel@buzzfeed.com>
2018-11-14 15:29:41 +0100

    fixed friend stuff

--- calculation.js
+++ calculation.js
@@ -6,51 +6,350 @@ module.exports = {
     switch (user) {
       case "Capricorn":
         {
-          if ((friendZodiac = "Pisces")) points += 7;
+          if ((friendZodiac = "Capricorn")) {
+            points += 7;
+          } else if ((friendZodiac = "Aquarius")) {
+            points += 7;
+          } else if ((friendZodiac = "Pisces")) {
+            points += 7;
+          } else if ((friendZodiac = "Aries")) {
+            points += 7;
+          } else if ((friendZodiac = "Taurus")) {
+            points += 7;
+          } else if ((friendZodiac = "Gemini")) {
+            points += 7;
+          } else if ((friendZodiac = "Cancer")) {
+            points += 7;
+          } else if ((friendZodiac = "Leo")) {
+            points += 7;
+          } else if ((friendZodiac = "Virgo")) {
+            points += 7;
+          } else if ((friendZodiac = "Libra")) {
+            points += 7;
+          } else if ((friendZodiac = "Scorpio")) {
+            points += 7;
+          } else if ((friendZodiac = "Sagittarius")) {
+            points += 7;
+          }
         }
         break;
       case "Aquarius":
         {
+          if ((friendZodiac = "Capricorn")) {
+            points += 7;
+          } else if ((friendZodiac = "Aquarius")) {
+            points += 7;
+          } else if ((friendZodiac = "Pisces")) {
+            points += 7;
+          } else if ((friendZodiac = "Aries")) {
+            points += 7;
+          } else if ((friendZodiac = "Taurus")) {
+            points += 7;
+          } else if ((friendZodiac = "Gemini")) {
+            points += 7;
+          } else if ((friendZodiac = "Cancer")) {
+            points += 7;
+          } else if ((friendZodiac = "Leo")) {
+            points += 7;
+          } else if ((friendZodiac = "Virgo")) {
+            points += 7;
+          } else if ((friendZodiac = "Libra")) {
+            points += 7;
+          } else if ((friendZodiac = "Scorpio")) {
+            points += 7;
+          } else if ((friendZodiac = "Sagittarius")) {
+            points += 7;
+          }
         }
         break;
       case "Pisces":
         {
+          if ((friendZodiac = "Capricorn")) {
+            points += 7;
+          } else if ((friendZodiac = "Aquarius")) {
+            points += 7;
+          } else if ((friendZodiac = "Pisces")) {
+            points += 7;
+          } else if ((friendZodiac = "Aries")) {
+            points += 7;
+          } else if ((friendZodiac = "Taurus")) {
+            points += 7;
+          } else if ((friendZodiac = "Gemini")) {
+            points += 7;
+          } else if ((friendZodiac = "Cancer")) {
+            points += 7;
+          } else if ((friendZodiac = "Leo")) {
+            points += 7;
+          } else if ((friendZodiac = "Virgo")) {
+            points += 7;
+          } else if ((friendZodiac = "Libra")) {
+            points += 7;
+          } else if ((friendZodiac = "Scorpio")) {
+            points += 7;
+          } else if ((friendZodiac = "Sagittarius")) {
+            points += 7;
+          }
         }
         break;
       case "Aries":
         {
+          if ((friendZodiac = "Capricorn")) {
+            points += 7;
+          } else if ((friendZodiac = "Aquarius")) {
+            points += 7;
+          } else if ((friendZodiac = "Pisces")) {
+            points += 7;
+          } else if ((friendZodiac = "Aries")) {
+            points += 7;
+          } else if ((friendZodiac = "Taurus")) {
+            points += 7;
+          } else if ((friendZodiac = "Gemini")) {
+            points += 7;
+          } else if ((friendZodiac = "Cancer")) {
+            points += 7;
+          } else if ((friendZodiac = "Leo")) {
+            points += 7;
+          } else if ((friendZodiac = "Virgo")) {
+            points += 7;
+          } else if ((friendZodiac = "Libra")) {
+            points += 7;
+          } else if ((friendZodiac = "Scorpio")) {
+            points += 7;
+          } else if ((friendZodiac = "Sagittarius")) {
+            points += 7;
+          }
         }
         break;
       case "Taurus":
         {
+          if ((friendZodiac = "Capricorn")) {
+            points += 7;
+          } else if ((friendZodiac = "Aquarius")) {
+            points += 7;
+          } else if ((friendZodiac = "Pisces")) {
+            points += 7;
+          } else if ((friendZodiac = "Aries")) {
+            points += 7;
+          } else if ((friendZodiac = "Taurus")) {
+            points += 7;
+          } else if ((friendZodiac = "Gemini")) {
+            points += 7;
+          } else if ((friendZodiac = "Cancer")) {
+            points += 7;
+          } else if ((friendZodiac = "Leo")) {
+            points += 7;
+          } else if ((friendZodiac = "Virgo")) {
+            points += 7;
+          } else if ((friendZodiac = "Libra")) {
+            points += 7;
+          } else if ((friendZodiac = "Scorpio")) {
+            points += 7;
+          } else if ((friendZodiac = "Sagittarius")) {
+            points += 7;
+          }
         }
         break;
       case "Gemini":
         {
+          if ((friendZodiac = "Capricorn")) {
+            points += 7;
+          } else if ((friendZodiac = "Aquarius")) {
+            points += 7;
+          } else if ((friendZodiac = "Pisces")) {
+            points += 7;
+          } else if ((friendZodiac = "Aries")) {
+            points += 7;
+          } else if ((friendZodiac = "Taurus")) {
+            points += 7;
+          } else if ((friendZodiac = "Gemini")) {
+            points += 7;
+          } else if ((friendZodiac = "Cancer")) {
+            points += 7;
+          } else if ((friendZodiac = "Leo")) {
+            points += 7;
+          } else if ((friendZodiac = "Virgo")) {
+            points += 7;
+          } else if ((friendZodiac = "Libra")) {
+            points += 7;
+          } else if ((friendZodiac = "Scorpio")) {
+            points += 7;
+          } else if ((friendZodiac = "Sagittarius")) {
+            points += 7;
+          }
         }
         break;
       case "Cancer":
         {
+          if ((friendZodiac = "Capricorn")) {
+            points += 7;
+          } else if ((friendZodiac = "Aquarius")) {
+            points += 7;
+          } else if ((friendZodiac = "Pisces")) {
+            points += 7;
+          } else if ((friendZodiac = "Aries")) {
+            points += 7;
+          } else if ((friendZodiac = "Taurus")) {
+            points += 7;
+          } else if ((friendZodiac = "Gemini")) {
+            points += 7;
+          } else if ((friendZodiac = "Cancer")) {
+            points += 7;
+          } else if ((friendZodiac = "Leo")) {
+            points += 7;
+          } else if ((friendZodiac = "Virgo")) {
+            points += 7;
+          } else if ((friendZodiac = "Libra")) {
+            points += 7;
+          } else if ((friendZodiac = "Scorpio")) {
+            points += 7;
+          } else if ((friendZodiac = "Sagittarius")) {
+            points += 7;
+          }
         }
         break;
       case "Leo":
         {
+          if ((friendZodiac = "Capricorn")) {
+            points += 7;
+          } else if ((friendZodiac = "Aquarius")) {
+            points += 7;
+          } else if ((friendZodiac = "Pisces")) {
+            points += 7;
+          } else if ((friendZodiac = "Aries")) {
+            points += 7;
+          } else if ((friendZodiac = "Taurus")) {
+            points += 7;
+          } else if ((friendZodiac = "Gemini")) {
+            points += 7;
+          } else if ((friendZodiac = "Cancer")) {
+            points += 7;
+          } else if ((friendZodiac = "Leo")) {
+            points += 7;
+          } else if ((friendZodiac = "Virgo")) {
+            points += 7;
+          } else if ((friendZodiac = "Libra")) {
+            points += 7;
+          } else if ((friendZodiac = "Scorpio")) {
+            points += 7;
+          } else if ((friendZodiac = "Sagittarius")) {
+            points += 7;
+          }
         }
         break;
       case "Virgo":
         {
+          if ((friendZodiac = "Capricorn")) {
+            points += 7;
+          } else if ((friendZodiac = "Aquarius")) {
+            points += 7;
+          } else if ((friendZodiac = "Pisces")) {
+            points += 7;
+          } else if ((friendZodiac = "Aries")) {
+            points += 7;
+          } else if ((friendZodiac = "Taurus")) {
+            points += 7;
+          } else if ((friendZodiac = "Gemini")) {
+            points += 7;
+          } else if ((friendZodiac = "Cancer")) {
+            points += 7;
+          } else if ((friendZodiac = "Leo")) {
+            points += 7;
+          } else if ((friendZodiac = "Virgo")) {
+            points += 7;
+          } else if ((friendZodiac = "Libra")) {
+            points += 7;
+          } else if ((friendZodiac = "Scorpio")) {
+            points += 7;
+          } else if ((friendZodiac = "Sagittarius")) {
+            points += 7;
+          }
         }
         break;
       case "Libra":
         {
+          if ((friendZodiac = "Capricorn")) {
+            points += 7;
+          } else if ((friendZodiac = "Aquarius")) {
+            points += 7;
+          } else if ((friendZodiac = "Pisces")) {
+            points += 7;
+          } else if ((friendZodiac = "Aries")) {
+            points += 7;
+          } else if ((friendZodiac = "Taurus")) {
+            points += 7;
+          } else if ((friendZodiac = "Gemini")) {
+            points += 7;
+          } else if ((friendZodiac = "Cancer")) {
+            points += 7;
+          } else if ((friendZodiac = "Leo")) {
+            points += 7;
+          } else if ((friendZodiac = "Virgo")) {
+            points += 7;
+          } else if ((friendZodiac = "Libra")) {
+            points += 7;
+          } else if ((friendZodiac = "Scorpio")) {
+            points += 7;
+          } else if ((friendZodiac = "Sagittarius")) {
+            points += 7;
+          }
         }
         break;
       case "Scorpio":
         {
+          if ((friendZodiac = "Capricorn")) {
+            points += 7;
+          } else if ((friendZodiac = "Aquarius")) {
+            points += 7;
+          } else if ((friendZodiac = "Pisces")) {
+            points += 7;
+          } else if ((friendZodiac = "Aries")) {
+            points += 7;
+          } else if ((friendZodiac = "Taurus")) {
+            points += 7;
+          } else if ((friendZodiac = "Gemini")) {
+            points += 7;
+          } else if ((friendZodiac = "Cancer")) {
+            points += 7;
+          } else if ((friendZodiac = "Leo")) {
+            points += 7;
+          } else if ((friendZodiac = "Virgo")) {
+            points += 7;
+          } else if ((friendZodiac = "Libra")) {
+            points += 7;
+          } else if ((friendZodiac = "Scorpio")) {
+            points += 7;
+          } else if ((friendZodiac = "Sagittarius")) {
+            points += 7;
+          }
         }
         break;
       case "Sagittarius":
         {
+          if ((friendZodiac = "Capricorn")) {
+            points += 7;
+          } else if ((friendZodiac = "Aquarius")) {
+            points += 7;
+          } else if ((friendZodiac = "Pisces")) {
+            points += 7;
+          } else if ((friendZodiac = "Aries")) {
+            points += 7;
+          } else if ((friendZodiac = "Taurus")) {
+            points += 7;
+          } else if ((friendZodiac = "Gemini")) {
+            points += 7;
+          } else if ((friendZodiac = "Cancer")) {
+            points += 7;
+          } else if ((friendZodiac = "Leo")) {
+            points += 7;
+          } else if ((friendZodiac = "Virgo")) {
+            points += 7;
+          } else if ((friendZodiac = "Libra")) {
+            points += 7;
+          } else if ((friendZodiac = "Scorpio")) {
+            points += 7;
+          } else if ((friendZodiac = "Sagittarius")) {
+            points += 7;
+          }
         }
         break;
     }
